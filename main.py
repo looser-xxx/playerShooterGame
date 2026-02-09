9bffd060af0f5c586f7ec7bcec7e0c2dda8d8abb
Suprabhat Maurya <itismaurya4769@gmail.com>
2026-02-09 14:29:15 +0530

    Implement shooting mechanics and gun rotation

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import random
+from enum import Flag
 
+from pygame import FLASH_BRIEFLY, key
 from pytmx.util_pygame import load_pygame
 
 from groups import AllSprites
 from player import Player
 from settings import *
 from sprites import *
+
+
+def loadAfterCooldown(eventTime, delay) -> bool:
+    """
+    Check if a cooldown period has passed.
+
+    Args:
+        eventTime (int): The timestamp when the event last occurred.
+        delay (int): The duration of the cooldown in milliseconds.
+
+    Returns:
+        bool: True if the cooldown has passed, False otherwise.
+    ""a
+    if (eventTime + delay) < pygame.time.get_ticks():
+        return True
+    else:
+        return False
 
 
 class Game:
@@ -17,6 +36,10 @@ class Game:
         self.setUpMap()
         self.runGame = True
         self.clock = pygame.time.Clock()
+        self.loadImages()
+
+    def loadImages(self):
+        self.bulletSurf = pygame.image.load("./images/gun/bullet.png").convert_alpha()
 
     def setUpPlayer(self, pos):
         self.playerSpeed = 400
@@ -27,6 +50,10 @@ class Game:
             pos,
         )
         self.gun = Gun(self.allSprites, self.player)
+        self.canShoot = True
+        self.shootTime = 0
+        self.shootDealy = 100
+        self.bulletSprite = pygame.sprite.Group()
 
     def checkEvents(self) -> None:
         """
@@ -59,6 +86,19 @@ class Game:
     def keyboardInput(self) -> None:
 
         keys = pygame.key.get_pressed()
+        if pygame.mouse.get_pressed()[0] and self.canShoot:
+            pos = self.gun.rect.center + self.gun.playerDirection * 50
+
+            Bullets(
+                (self.allSprites, self.bulletSprite),
+                self.bulletSurf,
+                pos,
+                self.gun.playerDirection,
+            )
+            self.canShoot = False
+            self.shootTime = pygame.time.get_ticks()
+        else:
+            self.canShoot = loadAfterCooldown(self.shootTime, self.shootDealy)
 
     def run(self) -> None:
         """
--- sprites.py
+++ sprites.py
@@ -1,3 +1,8 @@
+import random
+from math import atan2, degrees
+
+from pytmx.util_pygame import pygame_image_loader
+
 from settings import *
 
 
@@ -20,7 +25,7 @@ class Gun(pygame.sprite.Sprite):
     def __init__(self, groups, player) -> None:
         self.player = player
         self.distance = 140
-        self.playerDirection = pygame.Vector2(1, 0)
+        self.playerDirection = pygame.Vector2(0, 1)
 
         super().__init__(groups)
         self.gunSurf = pygame.image.load("./images/gun/gun.png").convert_alpha()
@@ -29,7 +34,50 @@ class Gun(pygame.sprite.Sprite):
             center=self.player.rect.center + self.playerDirection * self.distance
         )
 
+    def getDirection(self):
+        mousePos = pygame.Vector2(pygame.mouse.get_pos())
+        playerPos = pygame.Vector2(window[0] / 2, window[1] / 2)
+        self.playerDirection = (mousePos - playerPos).normalize()
+
+    def rotateGun(self):
+        angle = degrees(atan2(self.playerDirection.x, self.playerDirection.y)) - 90
+        if self.playerDirection.x > 0:
+            self.image = pygame.transform.rotozoom(self.gunSurf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gunSurf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
     def update(self, _):
+        self.getDirection()
+        self.rotateGun()
         self.rect.center = (
             self.player.rect.center + self.playerDirection * self.distance
         )
+
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, groups, surf, pos, direction) -> None:
+        super().__init__(groups)
+        self.surf = surf
+        self.image = self.surf
+        self.rect = self.image.get_frect(center=pos)
+        self.direction = direction
+        self.speed = 1000
+        self.rotationSpeed = 200
+        self.angle = random.randint(0, 360)
+        self.spawnTime = pygame.time.get_ticks()
+        self.lifeTime = 1000
+
+    def move(self, dt):
+        self.rect.center += self.speed * dt * self.direction
+
+    def rotate(self, dt):
+        self.image = pygame.transform.rotate(self.surf, self.angle)
+        self.angle += self.rotationSpeed * dt
+
+    def update(self, dt):
+        self.move(dt)
+        self.rotate(dt)
+        if pygame.time.get_ticks() > self.spawnTime + 1000:
+            self.kill()
+            print("hello")
